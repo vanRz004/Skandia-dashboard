08dc1e7d0c333e13af1f60b73667102b8fd2e358
Vanessa Ramirez <anvramirez004@gmail.com>
2025-09-04 20:31:52 -0500

    card list added

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
 import { GoalCardComponent } from '../../components/goal-card/goal-card';
+import { CardsListComponent } from '../../components/cards/cards-list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ Navbar, GoalCardComponent],
+  imports: [ Navbar, GoalCardComponent, CardsListComponent],
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
 })
